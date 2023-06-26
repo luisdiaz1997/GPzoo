38f4e49c155814c81faadbb31f073c43eb2e2180
Priyanka Shrestha <priyankaspace@gmail.com>
2023-06-25 19:47:14 -0700

    more changes

--- gpzoo/kernels.py
+++ gpzoo/kernels.py
@@ -1,84 +1,37 @@
 import torch
 from torch import cdist
 import numpy as np
-from abc import ABC, abstractmethod
+import torch.nn as nn
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
 
-class Kernel(ABC):
-  def __init__(self, X: torch.Tensor = None, variance=None, lengthscale = None, L = None) -> None:
-    self.sigma = torch.rand((1,1), dtype=torch.float, device=device, requires_grad= True) if variance is None else torch.tensor([variance**0.5], device=device, requires_grad=True, dtype=torch.float)
-    self.lengthscale = torch.rand((1,1), dtype=torch.float, device=device, requires_grad= True) if lengthscale is None else torch.tensor([lengthscale], device=device, requires_grad=True, dtype=torch.float)
-    self.X = X
-    self.distance = self.build_distance_mat(self.X, self.X) if self.X else None
-    self.parameters = [self.sigma, self.lengthscale]
-    self.L = L # Only for NSF_RBF kernel
+class RBF(nn.Module):
+  def __init__(self, sigma=1.0, lengthscale=2.0):
+    super().__init__()
+
+    self.sigma = nn.Parameter(torch.tensor(sigma))
+    self.lengthscale = nn.Parameter(torch.tensor(lengthscale))
+    
+  def forward(self, X, Z, diag=True):
+    if diag :
+        return (self.sigma**2).expand(X.size(0))
+    distance = torch.cdist(X, Z)
+    return self.sigma**2 * torch.exp(-0.5*(distance/self.lengthscale)**2)
+    
+
+class NSF_RBF(nn.Module):
+  def __init__(self, sigma=1.0, lengthscale=2.0, L=10):
+    super().__init__()
+
+    self.L = L
+    self.sigma = nn.Parameter(sigma*torch.ones((L, 1, 1)))
+    self.lengthscale = nn.Parameter(lengthscale*torch.ones((L, 1, 1)))
   
-  def build_distance_mat(self, X: torch.Tensor, Y: torch.Tensor) -> torch.Tensor:
-        return cdist(X, Y, p=self.norm)
+  def forward(self, X, Z, diag=False):
 
-  @property
-  @abstractmethod
-  def norm(self)-> int:
-    pass
+    if diag:
+      return ((self.sigma**2).squeeze())[:, None].expand(-1, X.size(0))
 
-  @abstractmethod
-  def build_kernel(self, distance)-> torch.Tensor:
-    pass
-
-  def params(self):
-    return self.parameters
-
-  def predict(self, X: torch.Tensor = None, Y: torch.Tensor = None):
-    '''
-    Uses distance matrix with the current data X or with unseen data Y to build kernel matrix.
-
-    Parameters:
-      X (torch.Tensor): new training
-      Y (torch.Tensor): test data
-        
-    Returns:
-      kernel (torch.Tensor): Kernel of K(X, X) or K(X, Y)
-    '''
-
-    if X is None:
-      if self.X is None: #If X is still None
-        raise RuntimeError('Failed to initialize training data, please initialize self.X or use custom X')
-            
-      X = self.X
-      distance = self.distance if Y is None else self.build_distance_mat(X, Y)
-        
-    else:
-        distance = self.build_distance_mat(X, X) if Y is None else self.build_distance_mat(X, Y)
-
-    return self.build_kernel(distance)
-    
-  def __call__(self, X : np.ndarray = None):
-    if self.X is None:
-        self.X = X
-        self.distance = self.build_distance_mat(self.X, self.X)
-    return self.predict()
-
-class RBF(Kernel):
-  def __init__(self, X: torch.Tensor = None, variance = None, lengthscale = None):
-    super(RBF, self).__init__(X, variance, lengthscale)
-    self.p = 2
-
-  @property
-  def norm(self):
-    return self.p
-
-  def build_kernel(self, distance: torch.Tensor) -> torch.Tensor:
-      return (self.sigma**2) * torch.exp(-0.5 * (distance/self.lengthscale)**2)
-
-class NSF_RBF(Kernel):
-  def __init__(self, X: torch.Tensor = None, variance = None, lengthscale = None, L = None):
-    super(NSF_RBF, self).__init__(X, variance, lengthscale, L)
-    self.p = 2
-
-  @property
-  def norm(self):
-    return self.p
-    
-  def build_kernel(self, distance: torch.Tensor) -> torch.Tensor:
+    distance = torch.cdist(X, Z)
+    distance = (distance[None, :, :]).expand(self.L, -1, -1)
     return self.sigma**2 * torch.exp(-0.5*(distance/self.lengthscale)**2)
