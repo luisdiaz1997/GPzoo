982c5047f1b4d54581dd0dea55080c07541586ae
Priyanka Shrestha <priyankaspace@gmail.com>
2024-04-16 00:13:05 -0700

    Added NSF preprocrssing

--- gpzoo/utilities.py
+++ gpzoo/utilities.py
@@ -9,6 +9,7 @@ from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
 from math import ceil
 import matplotlib.pyplot as plt
+from scipy import sparse
 
 
 '''
@@ -617,3 +618,42 @@ def train_batched(model, optimizer, X, y, device, steps=200, E=20, batch_size=10
             torch.cuda.empty_cache()
         
     return losses
+
+def deviancePoisson(X, sz=None):
+  """
+  X is matrix-like with observations in ROWs and features in COLs,
+  sz is a 1D numpy array that is same length as rows of X
+    sz are the size factors for each observation
+    sz defaults to the row means of X
+    set sz to 1.0 to have "no size factors" (ie Poisson for absolute counts instead of relative).
+
+  Note that due to floating point errors the results may differ slightly
+  between deviancePoisson(X) and deviancePoisson(X.todense()), use higher
+  floating point precision on X to reduce this.
+  """
+  dtp = X.dtype
+  X = X.astype("float64") #convert dtype to float64 for better numerics
+  if sz is None:
+    sz = np.ravel(X.mean(axis=1))
+  else:
+    sz = np.ravel(sz).astype("float64")
+  if len(sz)==1:
+    sz = np.repeat(sz,X.shape[0])
+  feature_sums = np.ravel(X.sum(axis=0))
+  try:
+    with np.errstate(divide='raise'):
+      ll_null = feature_sums*np.log(feature_sums/sz.sum())
+  except FloatingPointError:
+    raise ValueError("column with all zeros encountered, cannot compute deviance")
+  if sparse.issparse(X): #sparse scipy matrix
+    LP = sparse.diags(1./sz)@X
+    #LP is a NxJ matrix with same sparsity pattern as X
+    LP.data = np.log(LP.data) #log transform nonzero elements only
+    ll_sat = np.ravel(X.multiply(LP).sum(axis=0))
+  else: #dense numpy array
+    X = np.asarray(X) #avoid problems with numpy matrix objects
+    sz = np.expand_dims(sz,-1) #coerce to Nx1 array for broadcasting
+    with np.errstate(divide='ignore',invalid='ignore'):
+      ll_sat = X*np.log(X/sz) #sz broadcasts across rows of X
+    ll_sat = ll_sat.sum(axis=0, where=np.isfinite(ll_sat))
+  return (2*(ll_sat-ll_null)).astype(dtp)
